bf9f66f6f18de829ad881df8e8bcbc8bc9aa6b80
Jhedie <jeddiahawuku12@gmail.com>
2023-12-05 16:34:18 +0000

    feat: Add configuration for docker

--- awsConfig.js
+++ awsConfig.js
@@ -1,14 +1,14 @@
-const AWStest = require('aws-sdk');
+const AWStest = require("aws-sdk");
 
 // Configure AWS with credentials from environment variables
 AWStest.config.update({
-  accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
-  region: process.env.REACT_APP_AWS_REGION
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
-console.log(process.env.REACT_APP_AWS_ACCESS_KEY_ID);
-console.log(process.env.REACT_APP_AWS_SECRET_ACCESS_KEY);
-console.log(process.env.REACT_APP_AWS_REGION);
+console.log(process.env.AWS_ACCESS_KEY_ID);
+console.log(process.env.AWS_SECRET_ACCESS_KEY);
+console.log(process.env.AWS_REGION);
 
 module.exports = AWStest;
